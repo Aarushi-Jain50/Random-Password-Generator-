57f103b9eef64b663278b50b07884b7a701ef79f
Aarushi-Jain50 <aarushijain914@gmail.com>
2025-09-28 11:38:20 +0530

    First Commit

---
+++ RandomPass.js
@@ -0,0 +1,33 @@
+const passwordBox = document.getElementById("password"); // Match case
+const length = 12;
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const number = "0123456789";
+const symbol = "@#$%^&*()_+~|}{[]></-=";
+
+const allChars = upperCase + lowerCase + number + symbol;
+
+function createPassword() {
+    let password = "";
+    password += upperCase[Math.floor(Math.random() * upperCase.length)];
+    password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+    password += number[Math.floor(Math.random() * number.length)];
+    password += symbol[Math.floor(Math.random() * symbol.length)];
+
+    while (password.length < length) {
+        password += allChars[Math.floor(Math.random() * allChars.length)];
+    }
+    passwordBox.value = password;
+}
+
+function copyPassword() {
+    // Copy the value of the password box
+    navigator.clipboard.writeText(passwordBox.value)
+        .then(() => {
+            alert("Password copied to clipboard!");
+        })
+        .catch(err => {
+            console.error("Failed to copy: ", err);
+        });
+}
